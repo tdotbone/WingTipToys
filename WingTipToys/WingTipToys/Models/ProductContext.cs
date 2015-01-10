d26ea7cf662089ce7eea936a73a7b084aa9775a7
Tracy Hartzler <thartzler@gmail.com>
2015-01-09 17:41:20 -0800

    Created classes Product, Category and ProductContext

---
+++ WingTipToys/WingTipToys/Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace WingTipToys.Models
+{
+    public class ProductContext : DbContext  // ProductContext inherits from DbContext
+    {
+        public ProductContext() : base("WingTipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
